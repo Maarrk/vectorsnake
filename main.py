6ae97c631dfffcfd9f80476e3c036558cc230536
Marek Lukasiewicz <marqus34@gmail.com>
2017-05-22 11:19:58 +0200

    Changed ellipse to natural coordinate

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import vector_snake as vs
 import qtong
 
 triangle = [(-1, 0), (0, 2), (1, 0)]
-beniz = qtong.left_ball()
+beniz = [qtong.left_ball(float(i)/10) for i in range(11)]
 
 dwg = vs.Drawing(600, 600)
 dwg.set_scale(-3, 3, -1, 7)
@@ -23,9 +23,6 @@ fo.write(dwg.line((-1, 0), (2, 1)))
 
 dwg.set_color(0, 255, 255)
 fo.write(dwg.polyline(beniz))
-
-dwg.set_color(255, 0, 0)
-fo.write(dwg.polyline(qtong.intersection()))
 
 fo.write(dwg.end())
 fo.close()
--- qtong.py
+++ qtong.py
@@ -10,16 +10,17 @@ import my_math
 ellipse_perimeter = 7.93272
 
 
-def left_ball():
+def left_ball(s):
     end_point = my_math.resize(my_math.rotate(my_math.translate((-1, 2),
-                    (1.5, -1)), -math.atan(0.5)), (1, 0.6666666667))
+                                             (1.5, -1)),
+                               -math.atan(0.5)),
+                (1, 0.6666666667))
+
     end_angle = math.atan2(end_point[1], end_point[0])
 
-    circle = [(math.cos(float(i) / 10 * math.pi + end_angle),
-               math.sin(float(i) / 10 * math.pi + end_angle))
-              for i in range(11)]
-    scaled = [my_math.resize(point, (1, 1.5)) for point in circle]
-    rotated = [my_math.rotate(point, math.atan(0.5)) for point in scaled]
-    translated = [my_math.translate(point, (-1.5, 1)) for point in rotated]
+    circle = (math.cos(s * math.pi + end_angle), math.sin(s * math.pi + end_angle))
+    scaled = my_math.resize(circle, (1, 1.5))
+    rotated = my_math.rotate(scaled, math.atan(0.5))
+    translated = my_math.translate(rotated, (-1.5, 1))
 
     return translated
