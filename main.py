f384099208e84d0177ce48f7e988a5c366522b1e
Marek Lukasiewicz <marqus34@gmail.com>
2017-05-23 15:38:37 +0200

    Final shape made

--- main.py
+++ main.py
@@ -7,8 +7,7 @@
 import vector_snake as vs
 import qtong
 
-triangle = [(-1, 0), (0, 2), (1, 0)]
-beniz = [qtong.left_ball(float(i)/10) for i in range(11)]
+beniz = [qtong.qtong(i * 0.02) for i in range(51)]
 
 dwg = vs.Drawing(600, 600)
 dwg.set_scale(-3, 3, -1, 7)
@@ -18,8 +17,6 @@ fo = open('output.svg', 'w+')
 
 fo.write(dwg.begin())
 fo.write(dwg.draw_grid())
-
-fo.write(dwg.line((-1, 0), (2, 1)))
 
 dwg.set_color(0, 255, 255)
 fo.write(dwg.polyline(beniz))
--- qtong.py
+++ qtong.py
@@ -7,13 +7,30 @@
 import math
 import my_math
 
-ellipse_perimeter = 7.93272
+ellipse_perimeter_half = 3.96636
+
+
+def qtong(s):
+    # mirror image
+    if s > 0.5:
+        point = qtong(1-s)
+        return my_math.resize(point, (-1, 1))
+
+    s = s * 17.0743126536
+
+    if s < ellipse_perimeter_half:
+        return left_ball(s)
+    elif s <= ellipse_perimeter_half + 3:
+        return -1, s - ellipse_perimeter_half + 2
+    else:
+        return -math.cos(s - ellipse_perimeter_half - 3), 5 + math.sin(s - ellipse_perimeter_half - 3)
 
 
 def left_ball(s):
+    s = s / ellipse_perimeter_half
     end_angle = 0.4636476090
 
-    circle = (math.cos(s * math.pi + end_angle), math.sin(s * math.pi + end_angle))
+    circle = (-math.cos(s * math.pi - end_angle), math.sin(s * math.pi - end_angle))
     scaled = my_math.resize(circle, (1, 1.5))
     rotated = my_math.rotate(scaled, math.atan(0.5))
     translated = my_math.translate(rotated, (-1.5, 1))
